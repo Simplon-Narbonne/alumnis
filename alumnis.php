8cc85546dd5411d1c055a3bbc4adce366b972c25
Boyquotes <webmaster@linux-live-cd.org>
2016-05-02 09:56:37 +0200

    Ajout du fichier pour afficher le plugin alumnis dans l'administration wordpress

---
+++ alumnis.php
@@ -0,0 +1,12 @@
+< ?php
+/**
+ * Plugin Name: Alumnis
+ * Plugin URI: http://iness.simplon.co/alumnis.php
+ * Description: Permet de stocker via un formulaire d'enregistrement et ensuite d'afficher les anciens élèves sur une page du site
+ * Version: 1
+ * Author: Team Simplon Narbonne
+ * Author URI: http://iness.simplon.co
+ * License: GPL v3
+ */
+
+ 
